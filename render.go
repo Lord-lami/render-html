e9307f52ca9d720386dc1678eb762ebd59ea7626
Olamide Ifarajimi <lordelami@gmail.com>
2026-05-07 15:35:24 +0100

    Moved renderdata.go's logic to RenderBasic Function

--- render.go
+++ render.go
@@ -7,6 +7,7 @@ import (
 	"embed"
 	"html/template"
 	"reflect"
+	"strings"
 )
 
 // Type Ignored is used to store any value that should not be rendered as HTML.
@@ -96,8 +97,9 @@ func RenderBasic(chosenTemplateName string) RenderFunc {
 		}
 		templateData.Name = name
 		templateData.Value = data
-		var err error
-		dataHTML, err = renderData(templateData, chosenTemplateName)
+		var dataHtmlBuilder strings.Builder
+		err := TypeTemplates.ExecuteTemplate(&dataHtmlBuilder, chosenTemplateName, templateData)
+		dataHTML = template.HTML(dataHtmlBuilder.String())
 		if err != nil {
 			panic(err)
 		}
@@ -109,7 +111,7 @@ func RenderBasic(chosenTemplateName string) RenderFunc {
 // type RenderFunc.
 //
 // It is used internally for rendering composite types.
-// It panics if dType doesn't have a rendering function mapped to it in the 
+// It panics if dType doesn't have a rendering function mapped to it in the
 // render.TypeToRenderFuncMap map.
 func selectRenderFuncFor(dType reflect.Type) (renderFunc RenderFunc) {
 	renderFunc = TypeToRenderFuncMap[dType]
@@ -122,15 +124,15 @@ func selectRenderFuncFor(dType reflect.Type) (renderFunc RenderFunc) {
 		renderFunc = RenderMap
 	}
 	if renderFunc == nil {
-		panic("there is no render function for type " + dType.Name() + 
-		" in the render.TypeToRenderFuncMap map")
+		panic("there is no render function for type " + dType.Name() +
+			" in the render.TypeToRenderFuncMap map")
 	}
 	return
 }
 
 // Render renders the value passed to the data parameter. The name parameter is optional
 // and is by default used as the class of the HTML element.
-// It panics if the type of data doesn't have a rendering function mapped to it in the 
+// It panics if the type of data doesn't have a rendering function mapped to it in the
 // render.TypeToRenderFuncMap map or if data is an array, slice, struct or map that contains
 // elements that have types that don't have a rendering function mapped to them in the
 // render.TypeToRenderFuncMap map.
